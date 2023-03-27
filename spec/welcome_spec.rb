af8838f451e0f56aabb1ec084fbb4b2ffa1e30d0
Sydney Somerfield <sydneysomerfield@gmail.com>
2023-03-27 08:52:41 -0700

    added: trivial RSpec test to make sure CI is running tests

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -1,1 +1,8 @@
 # frozen_string_literal: true
+
+RSpec.describe 'a trival test to check CI tests are running' do
+  it 'checks array is empty' do
+    arr = []
+    expect(arr.length).to eq(0)
+  end
+end
