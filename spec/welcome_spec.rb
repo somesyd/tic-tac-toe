52d9c0d99f94c82cbb1a1be82fbdd46d014296a3
Sydney Somerfield <sydneysomerfield@gmail.com>
2023-03-27 08:57:42 -0700

    text change to test push actions in Github

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe 'a trival test to check CI tests are running' do
-  it 'checks array is empty' do
+  it 'checks array length is zero' do
     arr = []
     expect(arr.length).to eq(0)
   end
