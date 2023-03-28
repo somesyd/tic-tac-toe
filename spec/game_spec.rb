4a41fb1f8c932c58ff9f3484f4a51f6e7265720c
Sydney Somerfield <sydneysomerfield@gmail.com>
2023-03-28 14:31:28 -0700

    Add failing test for display blank board

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,14 +3,25 @@
 require 'game'
 require 'text'
 
-RSpec.describe 'Game' do
+RSpec.describe 'Game#play' do
   it 'displays a welcome message' do
-    output = instance_double(Display)
+    output = instance_double('display')
     allow(output).to receive(:text)
     game = Game.new(output)
 
     game.play
 
     expect(output).to have_received(:text).with(WELCOME_MESSAGE)
+  end
+
+  it 'displays a blank board' do
+    output = instance_double('display')
+    allow(output).to receive(:text)
+    allow(output).to receive(:display_board)
+    game = Game.new(output)
+
+    game.play
+
+    expect(output).to have_received(:display_board).with(Array.new(10, ''))
   end
 end
