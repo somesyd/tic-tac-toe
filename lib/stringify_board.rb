b4b2c7dc76826972cb03c4fd4b81edbe198102da
Sydney Somerfield <sydneysomerfield@gmail.com>
2023-03-30 16:25:09 -0700

    added: StringifyBoard class creates a pretty ascii board

--- lib/stringify_board.rb
+++ lib/stringify_board.rb
@@ -1,5 +1,53 @@
 # frozen_string_literal:true
 
 class StringifyBoard
-  
+  attr_reader :board_string
+
+  def initialize(board, vertical_char = '¦', horizontal_char = '-')
+    @board = board.dup
+    @vertical_char = vertical_char
+    @horizontal_char = horizontal_char
+    @vertical_padding = 5
+    @board_string = ''
+    build_board_string
+  end
+
+  def build_board_string
+    @board_string = build_player_row(0)
+    @board_string += horizontal_line
+    @board_string += build_player_row(1)
+    @board_string += horizontal_line
+    @board_string += build_player_row(2)
+  end
+
+  def build_player_row(row_num)
+    block = empty_vertical_line
+    block += vertical_player_line(row_num * 3)
+    block += empty_vertical_line
+    block
+  end
+
+  def empty_vertical_line
+    "#{' ' * @vertical_padding}#{@vertical_char}#{' ' * @vertical_padding}#{@vertical_char}\n"
+  end
+
+  def vertical_player_line(start_index)
+    line = ''
+    3.times do |i|
+      line += ' ' * player_padding
+      line += @board[start_index + i]
+      line += ' ' * player_padding
+      line += @vertical_char if i < 2
+    end
+    "#{line}\n"
+  end
+
+  def horizontal_line
+    width = (3 * @vertical_padding) + 2
+    "#{@horizontal_char * width}\n"
+  end
+
+  def player_padding
+    (@vertical_padding - 1) / 2
+  end
 end
