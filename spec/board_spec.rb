4e12fff0588cbe1ce4ee537eeafdac3e307bf04c
Sydney Somerfield <sydneysomerfield@gmail.com>
2023-03-31 13:05:25 -0700

    added: Board initialization test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal:true
+
+require 'board'
+
+RSpec.describe Board do
+  it 'begins as an array of indices 1-9' do
+    arr = '123456789'.chars
+    expect(subject.current_board).to eq(arr)
+  end
+end
