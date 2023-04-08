227be5685b97c8cd7eaedd5b3e30d30c548c57eb
Sydney Somerfield <sydneysomerfield@gmail.com>
2023-04-08 12:06:30 -0700

    fix: PrettyPrint was not clearin previous message before writing new one

--- lib/game.rb
+++ lib/game.rb
@@ -14,10 +14,11 @@ class Game
 
   def play
     display_welcome_message
-    until @board.win?
+    until @board.game_over?
       current_player = select_next_player(current_player)
       @turn.run(current_player)
     end
+    display_game_end_message
   end
 
   private
@@ -30,5 +31,18 @@ class Game
 
   def display_welcome_message
     @output.pretty_print_text(WELCOME_MESSAGE)
+  end
+
+  def display_game_end_message
+    if @board.win?
+      winner = determine_winner
+      @output.pretty_print_text("GAME OVER!!! #{winner.name} WINS!!!")
+    else
+      @output.pretty_print_text('GAME OVER!!! GAME is TIED!!!')
+    end
+  end
+
+  def determine_winner
+    @players.select { |player| player.token == @board.winner }[0]
   end
 end
